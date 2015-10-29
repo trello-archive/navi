203786ff5b72eb7ebeeaef6131a005f3117b2fb1
Dan Lew <danlew42@gmail.com>
2015-10-29 15:16:09 -0500

    Added RxNavi implementation

--- sample/src/main/java/com/trello/navi/sample/MainActivity.java
+++ sample/src/main/java/com/trello/navi/sample/MainActivity.java
@@ -1,12 +1,13 @@
 package com.trello.navi.sample;
 
+import android.os.Bundle;
 import android.widget.TextView;
 import butterknife.Bind;
 import butterknife.ButterKnife;
-import com.trello.navi.NaviActivity;
-import com.trello.navi.component.support.AbstractNaviActivity;
-import com.trello.navi.model.BundleBundle;
-import com.trello.navi.rx.RxNaviActivity;
+import com.trello.navi.Event;
+import com.trello.navi.NaviComponent;
+import com.trello.navi.component.support.NaviAppCompatActivity;
+import com.trello.navi.rx.RxNavi;
 import java.util.concurrent.TimeUnit;
 import rx.Observable;
 import rx.android.schedulers.AndroidSchedulers;
@@ -18,33 +19,32 @@ import rx.functions.Func1;
  *
  * Don't take this too seriously. This is just a fraction of what could be done with Navi.
  */
-public class MainActivity extends AbstractNaviActivity {
+public class MainActivity extends NaviAppCompatActivity {
 
   // This demonstrates we don't necessarily need a reference to the Activity itself in order
   // to hook into its lifecycle; the NaviActivity could be provided by anyone.
-  private final NaviActivity naviActivity = this;
+  private final NaviComponent naviComponent = this;
 
   @Bind(R.id.counter) TextView counter;
 
   public MainActivity() {
     // Instead of using onCreate, we can use Observables
-    RxNaviActivity.creating(naviActivity)
-        .subscribe(new Action1<BundleBundle>() {
-          @Override public void call(BundleBundle bundleBundle) {
-            setContentView(R.layout.main);
-            ButterKnife.bind(MainActivity.this);
-          }
-        });
+    RxNavi.observe(naviComponent, Event.CREATE).subscribe(new Action1<Bundle>() {
+      @Override public void call(Bundle bundle) {
+        setContentView(R.layout.main);
+        ButterKnife.bind(MainActivity.this);
+      }
+    });
 
     // Counter that operates on screen only while resumed; automatically ends itself on destroy
-    RxNaviActivity.resuming(naviActivity)
+    RxNavi.observe(naviComponent, Event.RESUME)
         .flatMap(new Func1<Void, Observable<Long>>() {
           @Override public Observable<Long> call(Void v) {
             return Observable.interval(1, TimeUnit.SECONDS)
-                .takeUntil(RxNaviActivity.pausing(naviActivity));
+                .takeUntil(RxNavi.observe(naviComponent, Event.PAUSE));
           }
         })
-        .takeUntil(RxNaviActivity.destroying(naviActivity))
+        .takeUntil(RxNavi.observe(naviComponent, Event.DESTROY))
         .startWith(-1L)
         .observeOn(AndroidSchedulers.mainThread())
         .subscribe(new Action1<Long>() {
